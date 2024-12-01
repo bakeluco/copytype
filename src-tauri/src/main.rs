b9d476197264c1a2fbbaacdff0a793f11af0f7f9
ProfXwing <50530928+ProfXwing@users.noreply.github.com>
2024-12-01 16:46:14 -0500

    keep window state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -115,6 +115,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             upload_book,
             get_book_list,
